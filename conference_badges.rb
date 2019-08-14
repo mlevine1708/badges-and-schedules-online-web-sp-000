1185d05681e215ebaf50ef1d74e6cf2a2ca8bb2d
Micah Levine <micah.levine@outlook.com>
2019-08-14 14:11:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,12 @@
-# Write your code here.
-attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-   "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-    "Hello, my name is #{attendee}.")
+    "Hello, my name is #{attendee}."
   end
 end
 
