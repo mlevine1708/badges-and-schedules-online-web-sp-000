b5633034a1937d70f25740f46fe738b220f43197
Micah Levine <micah.levine@outlook.com>
2019-08-14 14:05:17 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+name = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def badge_maker(name)
+  return ("Hello, my name is #{name}.")
+end
+
+def batch_badge_creator(array)
+  array = []
+  array.each do (name)
+    array.push("Hello, my name is #{name}.")
+  end
+  return array
+end
